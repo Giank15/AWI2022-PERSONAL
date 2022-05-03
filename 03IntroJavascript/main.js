cc942bac328b114476eea9e360f5f6dee46ad4fb
Giank15 <giancarlo_queirolo.15@outlook.es>
2022-05-03 18:48:05 -0500

    Primera practica de JavaScript: Tipos, estructuras y funciones

--- 03IntroJavascript/main.js
+++ 03IntroJavascript/main.js
@@ -1,5 +1,30 @@
 import './style.css'
 
+let valor1=1; //Valor cambiante o mutable 
+
+valor1=2;
+
+const iva=12; //Constante o valor inmutable
+
+var variableGlobal=0; //Variable global (deben evitarse)
+//variablex=6; //Cuando no se define, por default se definbe como var
+
+let valorMultiplicar=5;
+
+calcular.addEventListener('click', generarTablaMultiplicar(10));
+
+generarTablaMultiplicar(Number(multiplicar.value));
+
+function generarTablaMultiplicar(tabla){
+  for(let i=1;i<13;i++){
+    //console.log(i+'X'+valorMultiplicar+'='+i*valorMultiplicar);
+    //console.log(`${i} X ${valorMultiplicar} = ${i*valorMultiplicar}`);
+    //console.log(`${i} X ${Number(multiplicar.value)} = ${i*Number(multiplicar.value)}`);
+    console.log(`${i} X ${tabla} = ${i*tabla}`);
+  }
+}
+
+
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
