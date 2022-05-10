5fcb59b630d347b1f4bc5f2e8490f347c252fbfa
Giank15 <giancarlo_queirolo.15@outlook.es>
2022-05-10 18:57:23 -0500

    Tercera practica complementaria de JavaScript

--- complemento03/main.js
+++ complemento03/main.js
@@ -1,9 +1,10 @@
 import './style.css'
-
-for(let i=2;i<13;i++){
-  generarTablaMultiplicar(Number(i));
-  console.log(``);
-}
+tablasMultiplicar.addEventListener('click', ()=>{
+  for(let i=2;i<13;i++){
+    generarTablaMultiplicar(Number(i));
+    console.log(``);
+  }
+})
 function generarTablaMultiplicar(tabla){
   for(let i=1;i<13;i++){
     console.log(`${i} X ${tabla} = ${i*tabla}`);
@@ -11,8 +12,38 @@ function generarTablaMultiplicar(tabla){
 }
 
 generar.addEventListener('click', ()=>{
-  const conjunto = [Number(numb1),Number(numb2),Number(numb3),Number(numb4),Number(numb5)];
+  const conjunto = [Number(numb1.value),Number(numb2.value),Number(numb3.value),Number(numb4.value),Number(numb5.value)];
+  let auxiliar=0;
+  for(let i=0;i<5;i++){
+    if (conjunto[i]>auxiliar)
+    {
+      auxiliar=conjunto[i];
+    }
+  }
+  console.log(auxiliar);
+})
 
+intercambiar.addEventListener('click', ()=>{
+  let auxiliar;
+  auxiliar = texto1.value;
+  texto1.value = texto10.value;
+  texto10.value = auxiliar;
+
+  auxiliar = texto3.value;
+  texto3.value = texto8.value;
+  texto8.value = auxiliar;
+
+  auxiliar = texto5.value;
+  texto5.value = texto6.value;
+  texto6.value = auxiliar;
+
+  auxiliar = texto7.value;
+  texto7.value = texto4.value;
+  texto4.value = auxiliar;
+
+  auxiliar = texto9.value;
+  texto9.value = texto2.value;
+  texto2.value = auxiliar;
 })
 
 document.querySelector('#app').innerHTML = `
